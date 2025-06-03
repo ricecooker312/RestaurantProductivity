3e8fabfd5e7f45e2b2af4c29d304ac7a606ea12d
saihan <ricecooker312@gmail.com>
2025-06-02 21:00:26 -0500

    fixed backend errors

--- backend/dbconn.js
+++ backend/dbconn.js
@@ -1,6 +1,5 @@
 const path = require('path')
-const dotenv = require('dotenv')
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
+require('dotenv').config()
 
 const { MongoClient } = require('mongodb')
 const uri = process.env.MONGODB_CONNECTION_STR
