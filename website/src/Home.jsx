7e38fc9ab00f86da9d8010b706382b923c6df1f6
saihan <ricecooker312@gmail.com>
2025-07-03 22:49:28 -0500

    added support info in the support url

--- website/src/Home.jsx
+++ website/src/Home.jsx
@@ -7,7 +7,15 @@ function Home() {
 	return (
 		<div className='bg'>
 			<img src={logo} className='image' />
-			<h1 className='header'>Restaurant Productivity</h1>
+			<h1 className='header'>Mealstone</h1>
+
+			<p className='text'>
+				Mealstone is an app that allows you to build productivity through the completion of goals you set. However, unlike
+				most apps, it does it in a fun way that allows you to enjoy the process, and it encourages you to keep going on
+				your productivity journey. While you are completing goals, you get coins which you can use to buy items for your
+				restaurant, and the more goals you complete, the more items you will have and the more complete your restaurant
+				will be!
+			</p>
 
 			<a className='link' href='https://forms.gle/N7bjjyt2aohvC7eQ6' target="_blank">Ask a question</a>
 
